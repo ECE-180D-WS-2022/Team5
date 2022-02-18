0270744c34583963f8257c60a932e7d5fd0c89bb
Grace <lollypop631@gmail.com>
2022-02-17 16:37:54 -0800

    overcookedIRLv0.1

---
+++ OvercookedIRL/src/sprites.py
@@ -0,0 +1,275 @@
+import pygame
+from config import * 
+import math
+import random
+
+class Spritesheet:
+    def __init__(self, file):
+        self.sheet = pygame.image.load(file).convert()
+
+    def get_sprite(self, x, y, b_x, b_y, width, height):
+        sprite = pygame.Surface([width,height])
+        sprite.blit(self.sheet, (b_x,b_y), (x,y,width,height))
+        sprite.set_colorkey((255,0,255))
+        return sprite
+
+# Player inherits from pygame.sprite.Sprite (class in pygame module)
+class Player(pygame.sprite.Sprite):
+    def __init__(self, game, x, y):
+        self.game = game
+        self._layer = PLAYER_LAYER
+        # add player to all_sprites group of game object
+        self.groups = self.game.all_sprites
+
+        # call init method for inherited class
+        pygame.sprite.Sprite.__init__(self, self.groups)
+        
+        self.x = x * TILE_SIZE
+        self.y = y * TILE_SIZE
+        self.width = TILE_SIZE
+        self.height = 2*TILE_SIZE
+
+        self.x_change = 0
+        self.y_change = 0
+
+        self.facing = 'down'
+        self.animation_loop = 1
+
+        # self.image = pygame.Surface([self.width, self.height])
+        # self.image.fill((255,0,0))
+
+        # image = pygame.image.load("../img/amelia.png")
+        # self.image = pygame.Surface([self.width,self.height])
+        # self.image.set_colorkey(BLACK)  # black is transparent
+        # self.image.blit(image, (8,0))
+
+        self.image = self.game.character_idle_spritesheet.get_sprite(18*self.width,0,0,0,self.width,self.height)
+
+        # hit box of rect and image are the same
+        self.rect = self.image.get_rect()
+        self.rect.x = self.x
+        self.rect.y = self.y
+
+        self.right_run = [self.game.character_run_spritesheet.get_sprite(0*self.width,0,0,0,self.width, self.height),
+                    self.game.character_run_spritesheet.get_sprite(1*self.width,0,0,0,self.width, self.height),
+                    self.game.character_run_spritesheet.get_sprite(2*self.width,0,0,0,self.width, self.height),
+                    self.game.character_run_spritesheet.get_sprite(3*self.width,0,0,0,self.width, self.height),
+                    self.game.character_run_spritesheet.get_sprite(4*self.width,0,0,0,self.width, self.height),
+                    self.game.character_run_spritesheet.get_sprite(5*self.width,0,0,0,self.width, self.height)]
+        self.up_run = [self.game.character_run_spritesheet.get_sprite(6*self.width,0,0,0,self.width, self.height),
+                self.game.character_run_spritesheet.get_sprite(7*self.width,0,0,0,self.width, self.height),
+                self.game.character_run_spritesheet.get_sprite(8*self.width,0,0,0,self.width, self.height),
+                self.game.character_run_spritesheet.get_sprite(9*self.width,0,0,0,self.width, self.height),
+                self.game.character_run_spritesheet.get_sprite(10*self.width,0,0,0,self.width, self.height),
+                self.game.character_run_spritesheet.get_sprite(11*self.width,0,0,0,self.width, self.height)]
+        self.left_run = [self.game.character_run_spritesheet.get_sprite(12*self.width,0,0,0,self.width, self.height),
+                self.game.character_run_spritesheet.get_sprite(13*self.width,0,0,0,self.width, self.height),
+                self.game.character_run_spritesheet.get_sprite(14*self.width,0,0,0,self.width, self.height),
+                self.game.character_run_spritesheet.get_sprite(15*self.width,0,0,0,self.width, self.height),
+                self.game.character_run_spritesheet.get_sprite(16*self.width,0,0,0,self.width, self.height),
+                self.game.character_run_spritesheet.get_sprite(17*self.width,0,0,0,self.width, self.height)]
+        self.down_run = [self.game.character_run_spritesheet.get_sprite(18*self.width,0,0,0,self.width, self.height),
+                    self.game.character_run_spritesheet.get_sprite(19*self.width,0,0,0,self.width, self.height),
+                    self.game.character_run_spritesheet.get_sprite(20*self.width,0,0,0,self.width, self.height),
+                    self.game.character_run_spritesheet.get_sprite(21*self.width,0,0,0,self.width, self.height),
+                    self.game.character_run_spritesheet.get_sprite(22*self.width,0,0,0,self.width, self.height),
+                    self.game.character_run_spritesheet.get_sprite(23*self.width,0,0,0,self.width, self.height)]
+        self.right_idle = [self.game.character_idle_spritesheet.get_sprite(0*self.width,0,0,0,self.width, self.height),
+                    self.game.character_idle_spritesheet.get_sprite(1*self.width,0,0,0,self.width, self.height),
+                    self.game.character_idle_spritesheet.get_sprite(2*self.width,0,0,0,self.width, self.height),
+                    self.game.character_idle_spritesheet.get_sprite(3*self.width,0,0,0,self.width, self.height),
+                    self.game.character_idle_spritesheet.get_sprite(4*self.width,0,0,0,self.width, self.height),
+                    self.game.character_idle_spritesheet.get_sprite(5*self.width,0,0,0,self.width, self.height)]
+        self.up_idle = [self.game.character_idle_spritesheet.get_sprite(6*self.width,0,0,0,self.width, self.height),
+                self.game.character_idle_spritesheet.get_sprite(7*self.width,0,0,0,self.width, self.height),
+                self.game.character_idle_spritesheet.get_sprite(8*self.width,0,0,0,self.width, self.height),
+                self.game.character_idle_spritesheet.get_sprite(9*self.width,0,0,0,self.width, self.height),
+                self.game.character_idle_spritesheet.get_sprite(10*self.width,0,0,0,self.width, self.height),
+                self.game.character_idle_spritesheet.get_sprite(11*self.width,0,0,0,self.width, self.height)]
+        self.left_idle = [self.game.character_idle_spritesheet.get_sprite(12*self.width,0,0,0,self.width, self.height),
+                self.game.character_idle_spritesheet.get_sprite(13*self.width,0,0,0,self.width, self.height),
+                self.game.character_idle_spritesheet.get_sprite(14*self.width,0,0,0,self.width, self.height),
+                self.game.character_idle_spritesheet.get_sprite(15*self.width,0,0,0,self.width, self.height),
+                self.game.character_idle_spritesheet.get_sprite(16*self.width,0,0,0,self.width, self.height),
+                self.game.character_idle_spritesheet.get_sprite(17*self.width,0,0,0,self.width, self.height)]
+        self.down_idle = [self.game.character_idle_spritesheet.get_sprite(18*self.width,0,0,0,self.width, self.height),
+                        self.game.character_idle_spritesheet.get_sprite(19*self.width,0,0,0,self.width, self.height),
+                        self.game.character_idle_spritesheet.get_sprite(20*self.width,0,0,0,self.width, self.height),
+                        self.game.character_idle_spritesheet.get_sprite(21*self.width,0,0,0,self.width, self.height),
+                        self.game.character_idle_spritesheet.get_sprite(22*self.width,0,0,0,self.width, self.height),
+                        self.game.character_idle_spritesheet.get_sprite(23*self.width,0,0,0,self.width, self.height)]
+
+    def collide_counters(self, direction):
+        if direction == "x":
+            # checks if the rect of one sprite is inside the rect of another sprite
+            # False: do not want to delete sprite upon collision
+            hits = pygame.sprite.spritecollide(self, self.game.block_counters, False)
+            if hits:
+                if self.x_change > 0:   # moving right
+                    self.rect.x = hits[0].rect.left - self.rect.width
+                if self.x_change < 0:
+                    self.rect.x = hits[0].rect.right
+
+        if direction == "y":
+            hits = pygame.sprite.spritecollide(self, self.game.block_counters, False)
+            if hits:
+                if self.y_change > 0:   # moving down
+                    self.rect.y = hits[0].rect.top - self.rect.height
+                if self.y_change < 0:
+                    self.rect.y = hits[0].rect.bottom
+
+            hits = pygame.sprite.spritecollide(self, self.game.perspective_counters, False)
+            if hits:
+                if self.y_change > 0:   # moving down
+                    if(self.rect.y - hits[0].rect.top < 1):
+                        self.rect.y = hits[0].rect.top - self.rect.height
+
+
+    # player's update method called b/c game's update method calls all_sprites.update()
+    def update(self):
+        self.movement()
+        self.animate()
+
+        # add position change to player 
+        self.rect.x += self.x_change
+        self.collide_counters("x")
+        self.rect.y += self.y_change
+        self.collide_counters("y")
+
+        # print(self.x_change)
+        # print(self.rect.x)
+
+        self.y_change = 0
+        self.x_change = 0
+
+        print(self.rect.x, self.rect.y)
+
+
+    def movement(self):
+        # get all keys that have been pressed
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]:
+            self.x_change = -1 * PLAYER_SPEED
+            self.facing = 'left'
+        if keys[pygame.K_RIGHT]:
+            self.x_change = PLAYER_SPEED
+            self.facing = 'right'
+        if keys[pygame.K_UP]:
+            self.y_change = -1 * PLAYER_SPEED
+            self.facing = 'up'
+        if keys[pygame.K_DOWN]:
+            self.y_change = PLAYER_SPEED
+            self.facing = 'down'
+        
+    def animate(self):
+        if self.facing == "right":
+            if self.x_change == 0:
+                self.image = self.right_idle[math.floor(self.animation_loop)]
+                self.animation_loop += 0.1
+                if self.animation_loop >= 6:
+                    self.animation_loop = 1
+            else:
+                self.image = self.right_run[math.floor(self.animation_loop)]
+                self.animation_loop += 0.1
+                if self.animation_loop >= 6:
+                    self.animation_loop = 1
+        elif self.facing == "up":
+            if self.y_change == 0:
+                self.image = self.up_idle[math.floor(self.animation_loop)]
+                self.animation_loop += 0.1
+                if self.animation_loop >= 6:
+                    self.animation_loop = 1
+            else:
+                self.image = self.up_run[math.floor(self.animation_loop)]
+                self.animation_loop += 0.1
+                if self.animation_loop >= 6:
+                    self.animation_loop = 1
+        elif self.facing == "left":
+            if self.x_change == 0:
+                self.image = self.left_idle[math.floor(self.animation_loop)]
+                self.animation_loop += 0.1
+                if self.animation_loop >= 6:
+                    self.animation_loop = 1
+            else:
+                self.image = self.left_run[math.floor(self.animation_loop)]
+                self.animation_loop += 0.1
+                if self.animation_loop >= 6:
+                    self.animation_loop = 1
+        elif self.facing == "down":
+            if self.y_change == 0:
+                self.image = self.down_idle[math.floor(self.animation_loop)]
+                self.animation_loop += 0.1
+                if self.animation_loop >= 6:
+                    self.animation_loop = 1
+            else:
+                self.image = self.down_run[math.floor(self.animation_loop)]
+                self.animation_loop += 0.1
+                if self.animation_loop >= 6:
+                    self.animation_loop = 1
+
+class Counter(pygame.sprite.Sprite):
+    def __init__(self, game, x, y, type):
+
+        self.game = game
+        self._layer = COUNTER_LAYER
+        if(type == "E"):
+            self.groups = self.game.all_sprites, self.game.counters, self.game.perspective_counters
+        else:
+            self.groups = self.game.all_sprites, self.game.counters, self.game.block_counters
+        pygame.sprite.Sprite.__init__(self, self.groups)
+
+        self.x = x * TILE_SIZE
+        self.y = y * TILE_SIZE
+        self.width = TILE_SIZE
+        self.height = TILE_SIZE
+        
+        # self.image = pygame.Surface([self.width,self.height])
+        # self.image.fill((0,0,255))
+
+        # x, y, b_x, b_y, width, height
+        self.image = self.game.kitchen_spritesheet.get_sprite(green_counter[type][0],green_counter[type][1],0,0,self.width,self.height)
+        
+
+        self.rect = self.image.get_rect()
+        self.rect.x = self.x
+        self.rect.y = self.y
+
+class Cursor(pygame.sprite.Sprite):
+    def __init__(self, game, x, y):
+
+        self.game = game
+        self._layer = CURSOR_LAYER
+
+        self.groups = self.game.all_sprites
+        pygame.sprite.Sprite.__init__(self, self.groups)
+
+        self.x = x * TILE_SIZE
+        self.y = y * TILE_SIZE
+        self.width = TILE_SIZE
+        self.height = TILE_SIZE
+
+        self.image = pygame.Surface([self.width, self.height])
+        self.image.fill((255,255,0,128))
+
+        self.rect = self.image.get_rect()
+        self.rect.x = self.x
+        self.rect.y = self.y
+
+    def movement(self, x, y):
+        # get all keys that have been pressed
+        self.x = x%32
+        self.y = y%32
+
+    def update(self):
+        x = random.randint(0, WIN_WIDTH)
+        y = random.randint(0, WIN_HEIGHT)
+        self.movement(x, y)
+
+        # add position change to player 
+        self.rect.x = self.x * TILE_SIZE
+        self.rect.y = self.y * TILE_SIZE
+
+    
+
+
+
