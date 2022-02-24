9daf9642fd44eb139bb83e2ebba51afc4e7b3ee6
ryanldoan <69395683+ryanldoan@users.noreply.github.com>
2022-02-24 15:40:52 -0800

    Added click functionality

--- Color Detection/color.py
+++ Color Detection/color.py
@@ -21,50 +21,72 @@ def image_resize(image, width = None, height = None, inter = cv2.INTER_AREA):
 
     return resized
 
-cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
 
-font = cv2.FONT_HERSHEY_COMPLEX
-m = PyMouse()
+def mouseMovement():
+	cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+	cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+	cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
 
-while True:
-	_, frame = cap.read()
-	frame = cv2.flip(frame, 1)
+	font = cv2.FONT_HERSHEY_COMPLEX
+	m = PyMouse()
 
-	hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+	last_x = 99999
+	last_y = 99999
+	posCount = 0
+	clicked = False
 
-	lower_green = np.array([18, 67, 38])
-	upper_green = np.array([100, 255, 255])
-	mask_green = cv2.inRange(hsv, lower_green, upper_green)
+	while True:
+		_, frame = cap.read()
+		frame = cv2.flip(frame, 1)
 
-	kernel = np.ones((5,5), np.uint8)
-	mask_green = cv2.erode(mask_green, kernel)
+		hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-	contoursGreen, _ = cv2.findContours(mask_green, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+		lower_green = np.array([18, 67, 38])
+		upper_green = np.array([100, 255, 255])
+		mask_green = cv2.inRange(hsv, lower_green, upper_green)
 
-	for c in contoursGreen :
-		if cv2.contourArea(c) <= 400 :
-			continue
-		area = cv2.contourArea(c)
-		approx = cv2.approxPolyDP(c, 0.01*cv2.arcLength(c, True), True)
-		x, y, _, _ = cv2.boundingRect(c)
+		kernel = np.ones((5,5), np.uint8)
+		mask_green = cv2.erode(mask_green, kernel)
 
-		if 10 < len(approx) < 30:
-			print(len(approx))
-			cv2.putText(frame, "Circle", (x, y), font, 1, (0, 0, 0))
-			cv2.drawContours(frame, [approx], -1, (0, 255, 0), 3)
-			m.move(x, y)
+		contoursGreen, _ = cv2.findContours(mask_green, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-	frame = image_resize(frame, width = 400) 
-	cv2.imshow("frame", frame)
-	
-	key = cv2.waitKey(1)
-	if key == 27:
-		break
+		for c in contoursGreen :
+			if cv2.contourArea(c) <= 400 :
+				continue
+			area = cv2.contourArea(c)
+			approx = cv2.approxPolyDP(c, 0.01*cv2.arcLength(c, True), True)
 
-cap.release()
-cv2.destroyAllWindows()
+			if 10 < len(approx) < 30:
+				x, y, _, _ = cv2.boundingRect(c)
+				print(x,y)
+				cv2.putText(frame, "Circle", (x, y), font, 1, (0, 0, 0))
+				cv2.drawContours(frame, [approx], -1, (0, 255, 0), 3)
+				m.move(x, y)
+
+				if (abs(x - last_x) < 5) and (abs(y - last_y) < 5):
+					posCount += 1
+					if (posCount >= 20 and clicked == False):
+						m.click(x,y)
+						clicked = True
+				else:
+					posCount = 0
+				last_x = x
+				last_y = y
+					
+		if (clicked == True):
+			clicked = False
+			posCount = 0
+
+
+		frame = image_resize(frame, width = 400) 
+		cv2.imshow("frame", frame)
+		
+	# 	key = cv2.waitKey(1)
+	# 	if key == 27:
+	# 		break
+
+	# cap.release()
+	# cv2.destroyAllWindows()
 
 # https://pysource.com/2018/12/29/real-time-shape-detection-opencv-with-python-3/
 # https://medium.com/easyread/mouse-control-for-shooting-game-using-opencv-and-python-452c3446d1a3
