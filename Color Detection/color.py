890ee50cc47cb8425a452feb60f6b29b7fc81e60
ryanldoan <69395683+ryanldoan@users.noreply.github.com>
2022-03-01 19:23:05 -0800

    Mouse movement now uses object tracking

--- Color Detection/color.py
+++ Color Detection/color.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 from pymouse import PyMouse
 
+# Resize camera feed
 def image_resize(image, width = None, height = None, inter = cv2.INTER_AREA):
     dim = None
     (h, w) = image.shape[:2]
@@ -21,72 +22,65 @@ def image_resize(image, width = None, height = None, inter = cv2.INTER_AREA):
 
     return resized
 
+# Camera setup
+cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 
-def mouseMovement():
-	cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
-	cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
-	cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+# Mouse movement functionality
+m = PyMouse()
+last_x = 99999
+last_y = 99999
+posCount = 0
+clicked = False
 
-	font = cv2.FONT_HERSHEY_COMPLEX
-	m = PyMouse()
+# Tracker functionality
+ret,frame=cap.read()
+x,y,w,h = cv2.selectROI(frame)
+track_window = (x, y, w, h)
+roi = frame[y:y+h, x:x+w]
+hsv_roi =  cv2.cvtColor(roi, cv2.COLOR_BGR2HSV)
+mask = cv2.inRange(hsv_roi, np.array((0., 60.,32.)), np.array((180.,255.,255.)))
+roi_hist = cv2.calcHist([hsv_roi],[0],mask,[180],[0,180])
+cv2.normalize(roi_hist,roi_hist,0,255,cv2.NORM_MINMAX)
+term_crit = ( cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 1 )
 
-	last_x = 99999
-	last_y = 99999
-	posCount = 0
-	clicked = False
+# Resize camera
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
 
-	while True:
-		_, frame = cap.read()
-		frame = cv2.flip(frame, 1)
+while(1):
+    ret, frame = cap.read()
+    frame = cv2.flip(frame, 1)
+    
+    if ret == True:
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        dst = cv2.calcBackProject([hsv],[0],roi_hist,[0,180],1)
+        ret, track_window = cv2.meanShift(dst, track_window, term_crit)
+        x,y,w,h = track_window
+       
+        frame = cv2.rectangle(frame, (x,y), (x+w,y+h), (0,255,0),2)
+        m.move(x, y)
 
-		hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        if (abs(x - last_x) < 5) and (abs(y - last_y) < 5):
+            posCount += 1
+            if (posCount >= 20 and clicked == False):
+                m.click(x,y)
+                clicked = True
+        else:
+            posCount = 0
+        last_x = x
+        last_y = y
 
-		lower_green = np.array([18, 67, 38])
-		upper_green = np.array([100, 255, 255])
-		mask_green = cv2.inRange(hsv, lower_green, upper_green)
+        if (clicked == True):
+            clicked = False
+            posCount = 0
 
-		kernel = np.ones((5,5), np.uint8)
-		mask_green = cv2.erode(mask_green, kernel)
+        frame = image_resize(frame, width = 400) 
+        cv2.imshow('Camera',frame)
 
-		contoursGreen, _ = cv2.findContours(mask_green, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        k = cv2.waitKey(30) & 0xff
+        if k == 27:
+            break
+    else:
+        break
 
-		for c in contoursGreen :
-			if cv2.contourArea(c) <= 400 :
-				continue
-			area = cv2.contourArea(c)
-			approx = cv2.approxPolyDP(c, 0.01*cv2.arcLength(c, True), True)
-
-			if 10 < len(approx) < 30:
-				x, y, _, _ = cv2.boundingRect(c)
-				print(x,y)
-				cv2.putText(frame, "Circle", (x, y), font, 1, (0, 0, 0))
-				cv2.drawContours(frame, [approx], -1, (0, 255, 0), 3)
-				m.move(x, y)
-
-				if (abs(x - last_x) < 5) and (abs(y - last_y) < 5):
-					posCount += 1
-					if (posCount >= 20 and clicked == False):
-						m.click(x,y)
-						clicked = True
-				else:
-					posCount = 0
-				last_x = x
-				last_y = y
-					
-		if (clicked == True):
-			clicked = False
-			posCount = 0
-
-
-		frame = image_resize(frame, width = 400) 
-		cv2.imshow("frame", frame)
-		
-	# 	key = cv2.waitKey(1)
-	# 	if key == 27:
-	# 		break
-
-	# cap.release()
-	# cv2.destroyAllWindows()
-
-# https://pysource.com/2018/12/29/real-time-shape-detection-opencv-with-python-3/
-# https://medium.com/easyread/mouse-control-for-shooting-game-using-opencv-and-python-452c3446d1a3
+cv2.destroyAllWindows()
