2a9f4ed39a89cde6c95636c06439e21caf1c433b
ryanldoan <69395683+ryanldoan@users.noreply.github.com>
2022-04-07 16:44:00 -0700

    Exiting frame with object will stop tracking and clicking

--- OvercookedIRL/src/color.py
+++ OvercookedIRL/src/color.py
@@ -17,10 +17,11 @@ last_y = 99999
 posCount = 0
 clicked = False
 selected = False
-term_crit = ( cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.6 )
+term_crit = ( cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 1 )
+buffer = 0
 
 # Mouse Movement Stage
-while(1):
+while(1):   
     ret, frame = cap.read()
     frame = cv2.flip(frame, 1)
 
@@ -38,6 +39,8 @@ while(1):
                 mask = cv2.inRange(hsv_roi, np.array((0., 60.,32.)), np.array((180.,255.,255.)))
                 roi_hist = cv2.calcHist([hsv_roi],[0],mask,[180],[0,180])
                 cv2.normalize(roi_hist,roi_hist,0,255,cv2.NORM_MINMAX)
+                region = frame[y:y+h, x:x+w]
+                last_b, last_g, last_r = np.mean(region, axis=(0, 1))
                 selected = True
 
         if selected == True:
@@ -46,43 +49,59 @@ while(1):
             ret, track_window = cv2.meanShift(dst, track_window, term_crit)
             x,y,w,h = track_window
 
-            # if (ret == False):
-            #     print(term_crit)
-
-            if last_x == 99999:
-                frame = cv2.rectangle(frame, (x,y), (x+w,y+h), (0,255,0),3)
-
-            scaled_x = int(x * 3)
-            scaled_y = int(y * 3)
-
-            m.move(scaled_x, scaled_y)
-
-            if (abs(scaled_x - last_x) < 15) and (abs(scaled_y - last_y) < 15):
-                posCount += 1
-                frame = cv2.rectangle(frame, (frameX,frameY), (frameX+frameW,frameY+frameH), (0,255,0),3)
-                if (posCount >= 15 and clicked == False):
-                    m.click(scaled_x, scaled_y)
-                    clicked = True
-                elif (posCount >= 1 and posCount < 20 and clicked == False):
-                    m.move(last_x, last_y)
-                    scaled_x = last_x
-                    scaled_y = last_y      
+            if (buffer <= 10):
+                region = frame[y:y+h, x:x+w]
+                b, g, r = np.mean(region, axis=(0, 1))
+                buffer += 1
             else:
-                posCount = 0
-                frame = cv2.rectangle(frame, (x,y), (x+w,y+h), (0,255,0),3)
-            
-            last_x = scaled_x
-            last_y = scaled_y
+                ret, color_window = cv2.CamShift(dst, track_window, term_crit)
+                o,p,q,r = color_window
+                region = frame[p:p+r, o:o+q]
+                b, g, r = np.mean(region, axis=(0, 1))
 
-            if posCount == 0:
-                frameX = x
-                frameY = y
-                frameW = w
-                frameH = h
+            if (abs(b - last_b) < 30) and (abs(g - last_g) < 30) and (abs(r - last_r) < 30):
+                if last_x == 99999:
+                    frame = cv2.rectangle(frame, (x,y), (x+w,y+h), (0,255,0),3)
 
-            if (clicked == True):
-                clicked = False
-                posCount = 0
+                scaled_x = int(x * 3)
+                scaled_y = int(y * 3)
+
+                m.move(scaled_x, scaled_y)
+
+                if (abs(scaled_x - last_x) < 15) and (abs(scaled_y - last_y) < 15):
+                    posCount += 1
+                    frame = cv2.rectangle(frame, (frameX,frameY), (frameX+frameW,frameY+frameH), (0,255,0),3)
+
+                    if (posCount >= 15 and clicked == False):
+                        m.click(scaled_x, scaled_y)
+                        clicked = True
+                    elif (posCount >= 1 and posCount < 15 and clicked == False):
+                        m.move(last_x, last_y)
+                        scaled_x = last_x
+                        scaled_y = last_y
+                        if (posCount == 5):
+                            last_b = b
+                            last_g = g
+                            last_r = r
+                else:
+                    posCount = 0
+                    frame = cv2.rectangle(frame, (x,y), (x+w,y+h), (0,255,0),3)
+                
+                last_x = scaled_x
+                last_y = scaled_y
+
+                if posCount == 0:
+                    frameX = x
+                    frameY = y
+                    frameW = w
+                    frameH = h
+
+                if (clicked == True):
+                    clicked = False
+                    posCount = 0
+            else:
+                frame = cv2.rectangle(frame, (x,y), (x+w,y+h), (0,0,255),3)
+
 
         cv2.imshow('Mouse Movement',frame)
 
